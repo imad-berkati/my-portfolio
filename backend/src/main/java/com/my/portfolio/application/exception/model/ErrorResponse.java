5cccc5b6b3a457e69dc88dca19d2f2b2066e5937
Imad Berkati <berkati.imad@gmail.com>
2024-10-18 23:04:56 +0200

    Add Jacoco to backend and common modules

--- backend/src/main/java/com/my/portfolio/application/exception/model/ErrorResponse.java
+++ backend/src/main/java/com/my/portfolio/application/exception/model/ErrorResponse.java
@@ -2,8 +2,6 @@ package com.my.portfolio.application.exception.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.Instant;
-import lombok.Getter;
 
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(Instant timestamp, int status, String message) {}
---
+++ common/src/test/java/com/my/portfolio/JacocoCoverageTest.java
@@ -0,0 +1,14 @@
+package com.my.portfolio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+// TODO Delete me
+public class JacocoCoverageTest {
+
+  @Test
+  void test() {
+    assertEquals(1, 1);
+  }
+}
