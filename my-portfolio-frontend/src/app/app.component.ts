19143bb265f72319d935cea2ce06b2a8f18b16e4
Imad Berkati <berkati.imad@gmail.com>
2024-12-06 19:30:23 +0100

    add Menu responsive + create about-me and reviews component

--- my-portfolio-frontend/src/app/app.component.ts
+++ my-portfolio-frontend/src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent, MatSlideToggleModule],
+  imports: [RouterOutlet,
+      ProjectsComponent, AboutMeComponent, ReviewsComponent,
+      MatSlideToggleModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-portfolio-frontend';
 }
---
+++ my-portfolio-frontend/src/app/common/directives/dynamic-placeholder.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicPlaceholderDirective } from './dynamic-placeholder.directive';
+
+describe('DynamicPlaceholderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicPlaceholderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
