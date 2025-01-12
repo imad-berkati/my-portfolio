8be8ed545a644555518b9b73a673cbba2b688690
Imad Berkati <berkati.imad@gmail.com>
2025-01-12 12:55:01 +0100

    Refine project cards content

--- my-portfolio-frontend/src/app/pages/projects/projects.component.ts
+++ my-portfolio-frontend/src/app/pages/projects/projects.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { OnInit } from '../../../../node_modules/@angular/core/index';
 import { ProjectDto } from '../../../generated-api-client-v1/index';
 import { ProjectService } from '../../core/services/project.service';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatChipsModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
